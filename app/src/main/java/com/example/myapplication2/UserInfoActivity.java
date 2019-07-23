556b5f79906df145855bdbd914131bd390ee5d03
okrave <lomanto.calogero_luca_maria@educ.di.unito.it>
2019-07-23 14:26:59 +0200

    Cambiato la lista che riceve un utente da appgroup a appgroupfulldetail e settato graficamente il numero di gruppi in cui un utente è scritto

--- app/src/main/java/com/example/myapplication2/UserInfoActivity.java
+++ app/src/main/java/com/example/myapplication2/UserInfoActivity.java
@@ -17,13 +17,27 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.example.myapplication2.entities.AppGroup;
+import com.example.myapplication2.entities.GroupFullDetail;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.security.acl.Group;
+import java.util.ArrayList;
+import java.util.List;
 
 public class UserInfoActivity extends AppCompatActivity {
 
     TextView username;
     TextView email;
     TextView description;
+    TextView numberGroup;
 
+    List<GroupFullDetail> userGroups;
+
+    private Gson gson;
 
     Button nav_esplora;
     Button nav_profilo;
@@ -35,9 +49,10 @@ public class UserInfoActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
 
-        setUpNav();
+        setUpAllStuff();
         setUpAllViewStuff();
         loadUserGroups();
+
 
         username = (TextView) findViewById(R.id.info_username);
         email = (TextView) findViewById(R.id.info_email);
@@ -58,6 +73,34 @@ public class UserInfoActivity extends AppCompatActivity {
         }
     }
 
+    public void setUpAllStuff(){
+
+        //-----------------------------------------------GsonBuilder object
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.setDateFormat("yyyy/MM/dd");
+        gson = gsonBuilder.create();
+
+        //------------------------------------------------------Prendo i dati dell'utente loggato per costruire la pagina
+        username = (TextView) findViewById(R.id.info_username);
+        email = (TextView) findViewById(R.id.info_email);
+        description = (TextView) findViewById(R.id.info_description);
+
+        UserLogged utente = UserLogged.getInstance();
+
+        if (utente.isLogged()) {
+            username.setText(utente.getUserName());
+            email.setText(utente.getEmail());
+            description.setText("Ludovica ha 18 anni, è bionda, ha gli occhi verdi, " +
+                    "il colore della pelle molto chiaro ed è alta 1, 65 cm. E' una studentessa e frequenta da quattro anni" +
+                    " il liceo classico di Siena. E' una ragazza molto allegra, espansiva e ha molti amici. Nel suo tempo libero " +
+                    "ha due hobby in particolare: collezionare monete di tutti i Paesi del mondo e praticare lo sport dell'equitazione. ");
+        } else {
+            //not logged? -> logga !
+            goToLogInActivity();
+        }
+    }
+
+
 
     @Override
     protected void onResume() {
@@ -71,10 +114,22 @@ public class UserInfoActivity extends AppCompatActivity {
         }
     }
 
-    public void setUpNav() {
 
+    public void setUpAllViewStuff() {
+        numberGroup = (TextView) findViewById(R.id.numberGroups);
         nav_esplora = (Button) findViewById(R.id.user_nav_esplora);
         nav_profilo = (Button) findViewById(R.id.user_nav_profilo);
+        button_logout = (ImageButton) findViewById(R.id.button_logout);
+
+        button_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                UserLogged.getInstance().logout();
+                System.out.println(UserLogged.getInstance().isLogged);
+                Intent intent = new Intent(UserInfoActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
 
 
         nav_esplora.setOnClickListener(new View.OnClickListener() {
@@ -89,20 +144,6 @@ public class UserInfoActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(UserInfoActivity.this, UserInfoActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
-
-    public void setUpAllViewStuff() {
-        button_logout = (ImageButton) findViewById(R.id.button_logout);
-
-        button_logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                UserLogged.getInstance().logout();
-                System.out.println(UserLogged.getInstance().isLogged);
-                Intent intent = new Intent(UserInfoActivity.this, LoginActivity.class);
                 startActivity(intent);
             }
         });
@@ -123,6 +164,12 @@ public class UserInfoActivity extends AppCompatActivity {
         StringRequest stringRequest = new StringRequest(Request.Method.GET, server_url, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
+
+                Type typeOfList = new TypeToken<ArrayList<GroupFullDetail>>(){}.getType();
+                userGroups = (List<GroupFullDetail>)gson.fromJson(response,typeOfList);
+                System.out.println("GroupSize:" + userGroups.size());
+                numberGroup.setText(Integer.toString(userGroups.size()));
+                System.out.println(userGroups.get(0));
                 System.out.println(response);
                 requestQueue.stop();
             }
@@ -137,5 +184,7 @@ public class UserInfoActivity extends AppCompatActivity {
         });
         requestQueue.add(stringRequest);
     }
+
+
 
 }
